a653ef0a906f663f886923fed0d48b377a7c6a82
Raymond <raymondlugo64@gmail.com>
2025-08-26 16:24:47 -0400

    Corrigindo alguns bugs e mudando a estrutura do codigo.

--- python.py
+++ python.py
@@ -1,27 +1,3 @@
-while True:
-    print("\n=== Gerenciador de Tarefas ===")
-    print("1 - Adicionar tarefa")
-    print("2 - Listar tarefas")
-    print("3 - Marcar tarefa como concluída")
-    print("4 - Remover tarefa")
-    print("5 - Sair\n")
-
-    opcao = int(input("Escolha uma opçao: "))
-
-    if opcao == 1:
-        print("\nVocê escolheu adicionar uma tarefa.")
-    elif opcao == 2:
-        print("\nVocê escolheu listar uma tarefa.")
-    elif opcao == 3:
-        print("\nVocê escolheu concluir uma tarefa.")
-    elif opcao == 4:
-        print("\nVocê escolheu remover uma tarefa.")
-    elif opcao == 5:
-        print("\nSaindo do programa.")
-        break
-    else:
-        print("Opção Inválida, tente novamente")
-
 tarefas = []
 
 def adicionar_tarefa():
@@ -40,7 +16,7 @@ def listar_tarefas():
 
     for i, t in enumerate(tarefas, start=1):
         status = "Feita" if t['concluida'] else "Esperando"
-        print(f"{i}. {status} {t['descriçao']}")
+        print(f"{i}. {status} {t['descricao']}")
 
 def escolher_indice(prompt):
     if not tarefas:
@@ -64,4 +40,40 @@ def concluir_tarefa():
     if i is None:
         return
     tarefas[i]['concluida'] = True
-    print(f"Conluida: {tarefas[i]['descriçao']}")
+    print(f"Conluida: {tarefas[i]['descricao']}")
+
+def remover_tarefa():
+    i = escolher_indice("Numero da tarefa a remover: ")
+    if i is None:
+        return
+    removida = tarefas.pop(i)
+    print(f"Removida: {removida['descricao']}")
+
+
+while True:
+    print("\n=== Gerenciador de Tarefas ===")
+    print("1 - Adicionar tarefa")
+    print("2 - Listar tarefas")
+    print("3 - Marcar tarefa como concluída")
+    print("4 - Remover tarefa")
+    print("5 - Sair\n")
+
+    opcao = int(input("Escolha uma opçao: "))
+
+    if opcao == 1:
+        print("\nVocê escolheu adicionar uma tarefa.\n")
+        adicionar_tarefa()
+    elif opcao == 2:
+        print("\nVocê escolheu listar uma tarefa.\n")
+        listar_tarefas()
+    elif opcao == 3:
+        print("\nVocê escolheu concluir uma tarefa.\n")
+        escolher_indice()
+    elif opcao == 4:
+        print("\nVocê escolheu remover uma tarefa.\n")
+        remover_tarefa()
+    elif opcao == 5:
+        print("\nSaindo do programa.")
+        break
+    else:
+        print("Opção Inválida, tente novamente")
